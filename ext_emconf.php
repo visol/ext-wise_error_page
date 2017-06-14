6db5e9bb31078a5b53458dbf448ed6ddb123dbd0
Fabien Udriot <fabien.udriot@visol.ch>
2017-06-14 16:59:39 +0200

    [CLEANUP] Code identing

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,27 +1,27 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-	'title' => 'Speed up 404 error pages',
-	'description' => 'Help rendering faster 404 error pages',
-	'category' => 'misc',
-	'state' => 'stable',
-	'author' => 'Visol',
-	'author_email' => 'fabien.udriot@visol.ch',
-	'author_company' => 'Visol',
-    'autoload' => [
+    'title' => 'Speed up 404 error pages',
+    'description' => 'Help rendering faster 404 error pages',
+    'category' => 'misc',
+    'state' => 'stable',
+    'author' => 'Visol',
+    'author_email' => 'fabien.udriot@visol.ch',
+    'author_company' => 'Visol',
     'version' => '0.9.0-dev',
+    'autoload' => [
         'psr-4' => ['Visol\\WiseErrorPage\\' => 'Classes']
     ],
-	'constraints' =>
-	[
-		'depends' =>
-		[
-			'typo3' => '6.2.0-8.6.99',
+    'constraints' =>
+        [
+            'depends' =>
+                [
+                    'typo3' => '6.2.0-8.6.99',
+                ],
+            'conflicts' => '',
+            'suggests' =>
+                [
+                    'nc_staticfilecache' => '',
+                ],
         ],
-		'conflicts' => '',
-		'suggests' =>
-		[
-			'nc_staticfilecache' => '',
-        ],
-    ],
 ];
