ab3454abb375a59d42305b454d2053a4141fd361
Fabien Udriot <fabien.udriot@visol.ch>
2017-06-14 17:07:28 +0200

    [BUGFIX] Use globals array reference explicitly

--- ext_localconf.php
+++ ext_localconf.php
@@ -23,4 +23,4 @@ if (!defined('TYPO3_MODE')) {
 );
 
 # Flush internal cache
-$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] = 'Visol\WiseErrorPage\Cache\CacheHandler->clearCachePostProc';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] = 'Visol\WiseErrorPage\Cache\CacheHandler->clearCachePostProc';
